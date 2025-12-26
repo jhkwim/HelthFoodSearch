6b71db7c158257c4cac8ce05f790a316fd44c341
jhkwim <jhkwim@users.noreply.github.com>
2025-12-26 07:22:36 +0000

    deploy: 5898fad2b1ed54f293b325658598c0178aaca307

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "805468659"
+    serviceWorkerVersion: "31757237"
   }
 });
